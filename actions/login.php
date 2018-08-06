9ed5d2a1c57203b3996e5e2286abab9f4c5776f0
ZE3kr <ze3kr@tlo.xyz>
2018-08-06 22:09:00 +0800

    i18n

--- actions/login.php
+++ actions/login.php
@@ -9,7 +9,7 @@ if (isset($msg) && $msg != '') {echo '<div class="alert alert-warning" role="ale
 ?>
 <h1 class="login-h1 text-center"><?php echo _('Cloudflare CNAME/IP/NS Setup'); ?></h1>
 <form class="form-signin text-center" method="POST" action="">
-	<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+	<h1 class="h3 mb-3 font-weight-normal"><?php echo _('Please sign in'); ?></h1>
 	<label for="inputEmail" class="sr-only"><?php echo _('Your email address on cloudflare.com'); ?></label>
 	<input type="email" name="cloudflare_email" id="inputEmail" class="form-control" placeholder="<?php echo _('Your email address on cloudflare.com'); ?>" required autofocus>
 	<label for="inputPassword" class="sr-only"><?php echo _('Your password on cloudflare.com'); ?></label>
@@ -19,7 +19,7 @@ if (isset($msg) && $msg != '') {echo '<div class="alert alert-warning" role="ale
 			<input type="checkbox" value="remember-me" name="remember"> <?php echo _('Remember me'); ?>
 		</label>
 	</div>
-	<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	<button class="btn btn-lg btn-primary btn-block" type="submit"><?php echo _('Sign in'); ?></button>
 	<p class="mt-3 text-muted"><?php echo _('Use your existing account or create a new account here.'); ?></p>
 	<p class="text-muted"><?php echo _('We will not store any of your Cloudflare data'); ?></p>
 </form>
