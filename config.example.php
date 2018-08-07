b838ed6500f933ae9354f479a0bb76a39fdf2be4
ZE3kr <ze3kr@tlo.xyz>
2018-08-07 08:57:50 +0800

    Ready to release 1.1.0

--- config.example.php
+++ config.example.php
@@ -4,3 +4,4 @@ define('HOST_KEY', 'e9e4498f0584b7098692512db0c62b48');
 define('HOST_MAIL', 'ze3kr@example.com');
 // $page_title = "TlOxygen"; // Optional. Should not use HTML special character.
 // $tlo_path = '/'; // Optional. The installation path for this panel, ending with '/'. Required for HTTP/2 Push.
+// $is_debug = false; // Enable debug mode
