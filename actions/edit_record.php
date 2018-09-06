a1ef7aa244883cf5558c354283032fb038b5d228
ZE3kr <ze3kr@tlo.xyz>
2018-09-06 22:10:09 +0800

    Support CAA record

--- actions/edit_record.php
+++ actions/edit_record.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /*
  * Edit a record.
  */
@@ -16,13 +16,17 @@ if (isset($_POST['submit'])) {
 		$_POST['proxied'] = false;
 	}
 	$_POST['ttl'] = intval($_POST['ttl']);
+	$_POST['type'] = $dns_details->type;
 	if (isset($_POST['priority']) && $_POST['type'] == 'MX') {
 		$_POST['priority'] = intval($_POST['priority']);
 	} else {
 		$_POST['priority'] = 10;
 	}
+
+	include "record_data.php";
+
 	try {
-		if ($dns->updateRecordDetails($_GET['zoneid'], $_GET['recordid'], ['type' => $dns_details->type, 'name' => $_POST['name'], 'content' => $_POST['content'], 'ttl' => intval($_POST['ttl']), 'priority' => $_POST['priority'], 'proxied' => $_POST['proxied']])) {
+		if ($dns->updateRecordDetails($_GET['zoneid'], $_GET['recordid'], ['type' => $dns_details->type, 'name' => $_POST['name'], 'content' => $_POST['content'], 'ttl' => intval($_POST['ttl']), 'priority' => $_POST['priority'], 'proxied' => $_POST['proxied'], 'data' => $dns_data])) {
 			exit('<p class="alert alert-success" role="alert">' . _('Success') . '</p>');
 		} else {
 			echo '<p class="alert alert-danger" role="alert">' . _('Failed') . '</p>';
@@ -47,10 +51,28 @@ if (isset($msg)) {echo $msg;}
 				<option value="<?php echo $dns_details->type; ?>"><?php echo $dns_details->type; ?></option>
 			</select>
 		</div>
+
+		<?php if ($dns_details->type == 'CAA') {?>
+			<div class="form-group">
+				<label for="data_tag"><?php echo _('Tag'); ?></label>
+				<select name="data_tag" id="data_tag" class="form-control">
+					<option value="issue" selected="selected"><?php echo _('Only allow specific hostnames') ?></option>
+					<option value="issuewild"><?php echo _('Only allow wildcards') ?></option>
+					<option value="iodef"><?php echo _('Send violation reports to URL (http:, https:, or mailto:)') ?></option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="data_value"><?php echo _('Value'); ?></label>
+				<input type="text" name="data_value" id="data_value" value="<?php echo $dns_details->data->value; ?>" class="form-control">
+			</div>
+			<input type="hidden" name="data_flags" value="0">
+		<?php } else {?>
 		<div class="form-group">
 			<label for="doc-ta-1"><?php echo _('Record Content'); ?></label>
 			<textarea name="content" rows="5" id="doc-ta-1" class="form-control"><?php echo $dns_details->content; ?></textarea>
 		</div>
+		<?php }?>
+
 		<div class="form-group">
 			<label for="ttl">TTL</label>
 			<select name="ttl" id="ttl" class="form-control">
@@ -70,6 +92,7 @@ foreach ($ttl_translate as $_ttl => $_ttl_name) {
 			</select>
 		</div>
 		<?php }?>
+
 		<?php if ($dns_details->type == 'MX' || $dns_details->type == 'SRV') {?>
 		<div class="form-group">
 			<label for="priority"><?php echo _('Priority (Only for MX record)'); ?></label>
