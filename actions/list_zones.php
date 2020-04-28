239d2398ff4064d1df0018696d5dbbae2e5490ca
Guo Zeyu <ze3kr@icloud.com>
2020-04-28 13:12:07 +0800

    + Remove analytics + Add promotion in zone_list page

--- actions/list_zones.php
+++ actions/list_zones.php
@@ -13,11 +13,13 @@ if(!$no_api_key){
 <a href="?action=add" class="btn btn-primary float-sm-right mb-3 d-block"><?php echo _('Add Domain'); ?></a>
 <?php } ?>
 <h3 class="d-none d-sm-block"><?php echo _('Home'); ?></h3>
-
 <?php if($no_api_key){
-	echo '<div class="alert alert-warning" role="alert">' . _('No Host API key found. You cannot add new domain to this service.') . '</div>';
+	if(isset($tlo_promotion_header)){
+		echo $tlo_promotion_header;
+	} else {
+		echo '<div class="alert alert-warning" role="alert">' . _('No Host API key found. You cannot add new domain to this service.') . '</div>';
+	}
 } ?>
-
 <table class="table table-striped">
 	<thead>
 	<tr>
@@ -50,7 +52,6 @@ foreach ($zones_data->result as $zone) {
 				</button>
 				<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 					<a class="dropdown-item" href="https://dash.cloudflare.com/" target="_blank">{$_translate_manage_dns}</a>
-					<a class="dropdown-item" href="?action=analytics&amp;domain={$zone->name}&amp;zoneid={$zone->id}">{$_translate_analytics}</a>
 				</div>
 			</div>
 			{$zone->name}
@@ -66,7 +67,6 @@ HTML;
 				</button>
 				<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 					<a class="dropdown-item" href="?action=zone&amp;domain={$zone->name}&amp;zoneid={$zone->id}">{$_translate_manage_dns}</a>
-					<a class="dropdown-item" href="?action=analytics&amp;domain={$zone->name}&amp;zoneid={$zone->id}">{$_translate_analytics}</a>
 					<a class="dropdown-item" href="?action=security&amp;domain={$zone->name}&amp;zoneid={$zone->id}">{$_translate_security}</a>
 				</div>
 			</div>
@@ -84,18 +84,12 @@ HTML;
 HTML;
 	if (property_exists($zone, 'name_servers')) {
 		echo '<a href="https://dash.cloudflare.com/" target="_blank" class="btn btn-secondary btn-sm" data-toggle="tooltip" data-placement="top" title="' . _('This domain only supports NS setup. And you should manage DNS records on Cloudflare.com.') . '">' . _('Manage DNS') . '</a>';
-		echo <<<HTML
-<a href="?action=analytics&amp;domain={$zone->name}&amp;zoneid={$zone->id}" class="btn btn-dark btn-sm">{$_translate_analytics}</a>
-HTML;
 	} else {
 		echo <<<HTML
 <a href="?action=zone&amp;domain={$zone->name}&amp;zoneid={$zone->id}" class="btn btn-secondary btn-sm">{$_translate_manage_dns}</a>
 HTML;
 		echo <<<HTML
-<a href="?action=analytics&amp;domain={$zone->name}&amp;zoneid={$zone->id}" class="btn btn-dark btn-sm">{$_translate_analytics}</a>
-HTML;
-		echo <<<HTML
-<a href="?action=security&amp;domain={$zone->name}&amp;zoneid={$zone->id}" class="btn btn-secondary btn-sm">{$_translate_security}</a>
+<a href="?action=security&amp;domain={$zone->name}&amp;zoneid={$zone->id}" class="btn btn-dark btn-sm">{$_translate_security}</a>
 HTML;
 
 	}
@@ -103,7 +97,10 @@ HTML;
 }
 ?>
 	</tbody>
-</table><?php
+</table>
+<?php if($no_api_key && isset($tlo_promotion_footer)){
+	echo $tlo_promotion_footer;
+}
 if (isset($zones_data->result_info->total_pages)) {
 	$previous_page = '';
 	$next_page = '';
